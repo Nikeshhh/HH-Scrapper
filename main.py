9ee3d0b27d10a78932b12c1fefc04eac79a9b6e4
Nikeshhh <WityazKajit@gmail.com>
2023-03-27 14:46:00 +0300

    Changed main function so token can be inputed

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import os
 
 
 def main():  # Основная функция для работы бота
-    my_vk_api = api_vk.MyVkApi(os.getenv('VK_API_TOKEN'))  # Авторизация бота в api через токен
-    my_parser = parser.MyHHParser('https://krasnodar.hh.ru/search/vacancy?text=python+junior&area=53', 10, my_vk_api)
+    token = input('Input a token: ')
+    my_vk_api = api_vk.MyVkApi(token)  # Авторизация бота в api через токен
+    my_parser = parser.MyHHParser('https://krasnodar.hh.ru/search/vacancy?text=python+junior&area=53', 28800, my_vk_api)
     my_parser.get_cycle()  # Основной цикл работы бота
 
 
